9efe930a56a5e4bd007ba97e2c438f52447599ab
takiguchi-yu <y.takiguti@gmail.com>
2024-06-03 02:01:45 +0900

    chore: :building_construction: PrismaService implements

--- backend/src/todo/todo.module.ts
+++ backend/src/todo/todo.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaService],
   controllers: [TodoController],
   providers: [TodoService],
 })
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaService],
   controllers: [UserController],
   providers: [UserService],
 })
